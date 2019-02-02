684e334f44eb554e4c1079e7b136f0536afdfd7d
Mikolaj Torz <mptorz@icloud.com>
2019-02-02 15:37:05 +0000

    ready

--- src/index.js
+++ src/index.js
@@ -4,14 +4,10 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './components/App';
-import * as API from './utils/_DATA';
 import reducer from './reducers';
 import middleware from './middleware';
 
 const store = createStore(reducer, middleware);
-window.debugAPI = API;
-window.debugStore = store;
-
 ReactDOM.render(
   <Provider store={store}>
     <App />
