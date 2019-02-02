407bf32496d5a3cb85f26f91f537aa6236af1e62
Mikolaj Torz <mptorz@icloud.com>
2019-02-02 15:21:06 +0000

    ready for checks

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,5 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { List, Avatar } from 'antd';
 
-export default function Leaderboard() {
-  return <div />;
-}
+const Leaderboard = props => {
+  return (
+    <div
+      style={{ flexDirection: 'column', display: 'flex', alignItems: 'center' }}
+    >
+      <List
+        style={{ width: 500 }}
+        bordered
+        itemLayout="horizontal"
+        dataSource={props.users}
+        renderItem={item => (
+          <List.Item>
+            <List.Item.Meta
+              title={item.name}
+              avatar={<Avatar src={item.avatarURL} />}
+              description={`${item.name} asked ${
+                item.questions.length
+              } questions and answered ${
+                Object.keys(item.answers).length
+              } questions. That gives him/her a score of ${item.score}`}
+            />
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+};
+
+const mapStateToProps = ({ users }) => {
+  const usersWithScore = Object.values(users).map(user =>
+    Object.assign(user, {
+      score: Object.keys(user.answers).length + user.questions.length,
+    })
+  );
+  const usersSorted = usersWithScore.sort((a, b) => b.score - a.score);
+  return { users: usersSorted };
+};
+
+export default connect(mapStateToProps)(Leaderboard);
